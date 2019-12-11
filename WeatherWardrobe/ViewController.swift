e866407c80aeebd835641e56cc4e547ad04df11c
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-12-10 21:39:52 -0800

    initial commit

---
+++ WeatherWardrobe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherWardrobe
+//
+//  Created by Gonzalo Birrueta on 12/10/19.
+//  Copyright © 2019 Gonzalo Birrueta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
